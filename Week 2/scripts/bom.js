1f40cb87516bc1dd051eaec7b2fe53fb7a89cd80
AaronStoor <Aaronbstoor@gmail.com>
2025-03-12 21:42:21 -0600

    completed the bom.js assignment

--- Week 2/scripts/bom.js
+++ Week 2/scripts/bom.js
@@ -1,9 +1,18 @@
-const input = document.querySelector("#favchap");
-const button = document.querySelector("#button");
-const list = document.querySelector("_______");
-const li = document.querySelector("li");
-const deleteButton = document.querySelector("button");
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
+
+if (input.value.trim() !== '') {
+    const input = document.querySelector("#favchap");
+    const button = document.querySelector("#button");
+    const list = document.querySelector("_______");
+    const li = document.querySelector("li");
+    const deleteButton = document.querySelector("button");
+    input.value = '';
+    li.textContent = input.value;
+    deleteButton.textContent = '❌';
+    li.append(deleteButton);
+    list.append(li);
+
+    button.addEventListener("click", function () {
+        list.removeChild(li);
+        input.focus();
+    });
+}

546a840906fe86de3964c38fe5a30b21a64ff542
AaronStoor <Aaronbstoor@gmail.com>
2025-03-12 21:37:12 -0600

    completed the bom htmnl section

---
+++ Week 2/scripts/bom.js
@@ -0,0 +1,9 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("#button");
+const list = document.querySelector("_______");
+const li = document.querySelector("li");
+const deleteButton = document.querySelector("button");
+li.textContent = input.value;
+deleteButton.textContent = '❌';
+li.append(deleteButton);
+list.append(li);
